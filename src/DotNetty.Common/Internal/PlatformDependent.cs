8a40ea42bef43d9f76011b855ba569e4a4416ade
mkim <mkim@microsoft.com>
2017-11-06 18:13:58 -0800

    FixedChannelPool

--- src/DotNetty.Common/Internal/PlatformDependent.cs
+++ src/DotNetty.Common/Internal/PlatformDependent.cs
@@ -16,6 +16,8 @@ namespace DotNetty.Common.Internal
         public static IQueue<T> NewFixedMpscQueue<T>(int capacity) where T : class => new MpscArrayQueue<T>(capacity);
 
         public static IQueue<T> NewMpscQueue<T>() where T : class => new CompatibleConcurrentQueue<T>();
+        
+        public static IDeque<T> NewDeque<T>() where T :class => new LockFreeWorkStealingQueue<T>();
 
         public static ILinkedQueue<T> SpscLinkedQueue<T>() where T : class => new SpscLinkedQueue<T>();
 
