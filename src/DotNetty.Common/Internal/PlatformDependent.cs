cb25a124cddfb7dfe5feedc94c20dfe085c28ee4
mkim <mkim@microsoft.com>
2017-11-17 15:25:59 -0800

    new queue implementation. Fixes in localchannel

--- src/DotNetty.Common/Internal/PlatformDependent.cs
+++ src/DotNetty.Common/Internal/PlatformDependent.cs
@@ -17,7 +17,7 @@ namespace DotNetty.Common.Internal
 
         public static IQueue<T> NewMpscQueue<T>() where T : class => new CompatibleConcurrentQueue<T>();
         
-        public static IDeque<T> NewDeque<T>() where T :class => new LockFreeWorkStealingQueue<T>();
+        public static IDeque<T> NewDeque<T>() where T :class => new WorkStealingQueue<T>();
 
         public static ILinkedQueue<T> SpscLinkedQueue<T>() where T : class => new SpscLinkedQueue<T>();
 
