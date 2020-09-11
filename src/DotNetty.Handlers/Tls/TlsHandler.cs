8140f875eee84dee25167715f5830c2207dd4e20
Maxim Kim <mkim@microsoft.com>
2020-09-10 19:01:27 -0700

    fix AddBufferToOutput corner case in TlsHandler

--- src/DotNetty.Handlers/Tls/TlsHandler.cs
+++ src/DotNetty.Handlers/Tls/TlsHandler.cs
@@ -429,6 +429,11 @@ namespace DotNetty.Handlers.Tls
                             break;
                         }
                         int read = currentReadFuture.Result;
+                        if (read == 0)
+                        {
+                            //Stream closed
+                            return;
+                        }
                         AddBufferToOutput(outputBuffer, read, output);
                     }
                     outputBuffer = ctx.Allocator.Buffer(FallbackReadBufferSize);
